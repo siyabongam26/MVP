9030fec7eef022934f811e0c87fa8389a8e61f83
Siyabongam26 <siyabongam26@gmail.com>
2024-06-11 04:37:02 -0700

    update

--- models/task.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const TaskSchema = new mongoose.Schema({
-  task: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-});
-
-module.exports = mongoose.model('Task', TaskSchema);
